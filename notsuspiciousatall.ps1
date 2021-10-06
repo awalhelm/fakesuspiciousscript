df471083f9cf1b2503f01597977d16dc2a3cdef9
awalhelm <70651280+awalhelm@users.noreply.github.com>
2021-10-06 16:17:19 -0400

    eited file extension for eicar.com

--- notsuspiciousatall.ps1
+++ notsuspiciousatall.ps1
@@ -3,7 +3,7 @@ Restart-Service Spooler
 Write-Output "Spooler restarted at $(get-date)" >> C:\Users\m.smith\timestamps.log
 mkdir "C:\gfhdf4536fghf"
 $url = "https://amtso.eicar.org/eicar.com"
-$output = "c:\gfhdf4536fghf\eicar.com.ps1"
+$output = "c:\gfhdf4536fghf\eicar.com"
 Write-Output "downloading eicar.com at $(get-date)" >> C:\Users\m.smith\timestamps.log
 Invoke-WebRequest -Uri $url -OutFile $output
 Write-Output "END at $(get-date)" >> C:\Users\m.smith\timestamps.log
