47d20253e1b0aab0ea615f030cbd542e2f5dd09c
awalhelm <70651280+awalhelm@users.noreply.github.com>
2021-10-06 15:35:13 -0400

    script not malicious enough, downloading eicar.com file

--- notsuspiciousatall.ps1
+++ notsuspiciousatall.ps1
@@ -1,2 +1,9 @@
+Write-Output "STARING at $(get-date)" >> C:\Users\m.smith\timestamps.log
 Restart-Service Spooler
-Write-Output "run at $(get-date)" >> C:\Users\m.smith\timestamps.log
+Write-Output "Spooler restarted at $(get-date)" >> C:\Users\m.smith\timestamps.log
+mkdir "C:\gfhdf4536fghf"
+$url = "https://amtso.eicar.org/eicar.com"
+$output = "c:\gfhdf4536fghf\eicar.com.ps1"
+Write-Output "downloading eicar.com at $(get-date)" >> C:\Users\m.smith\timestamps.log
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-Output "END at $(get-date)" >> C:\Users\m.smith\timestamps.log
